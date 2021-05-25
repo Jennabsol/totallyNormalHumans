2f8d76ff2173471493d2edb38ff5601367a4b7bd
Jenna Solis <jennabsol@gmail.com>
2021-05-25 00:04:57 -0500

    cleaned up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
 import './App.css';
-import { ThemeProvider } from './context/theme/ThemeProvider';
+
 import { ContentCard } from './components/contentCard/ContentCard';
+import { ThemeProvider } from './context/theme/ThemeProvider';
 import biker from './assets/images/biker.png';
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider>
-        <ContentCard
-          imageUrl={biker}
-          headline="Live Your Life Now"
-          body="After MitraClip, you can resume usual activity
+        <>
+          <ContentCard
+            imageUrl={biker}
+            headline="Live Your Life Now"
+            body="After MitraClip, you can resume usual activity
                         immediately. Recovery from OHS takes months."
-          button={{
-            text: 'Get the Process Started',
-            onClick: () => console.log('hello')
-          }}
-        />
+          />
+          <ContentCard
+            imageUrl="https://freesvg.org/img/robot.png"
+            headline="Live Your Human Life Now"
+            body="Trust me! I am a totally normal human!"
+            button={{
+              text: 'Human not Robot',
+              onClick: () => console.log('beep boop')
+            }}
+          />
+        </>
       </ThemeProvider>
     </div>
   );
